3f0c43eb479fd655146b3250e79bda54b03efd79
00AliNuri <126083087+00AliNuri@users.noreply.github.com>
2023-03-06 00:03:00 +0400

    custom reverse method is done

---
+++ CustomReverseMethod/Program.cs
@@ -0,0 +1,9 @@
+﻿void Reverse(int[] arr, int rever = 0)
+{
+    rever = arr.Length;
+    for (int i = rever - 1; i < arr.Length + rever; i--)
+    {
+        Console.WriteLine(arr[i]);
+    }
+}
+Reverse(new int[] { 9, 8, 7, 98, 76, 19 });
